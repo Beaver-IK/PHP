1b79ed73e36eb570a068febdafff850053c67e98
Ivan Kontsov <ivan.kontsov@gmail.com>
2025-02-03 00:18:35 +0300

    index

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php
 
-echo 'Hello World';
+class Foo
+{
+    public $bar = 'Я bar.';
+    public $arr = array('Я A.', 'Я B.', 'Я C.');
+    public $r   = 'Я r.';
+}
+$foo = new Foo();
+$bar = 'bar';
+$baz = ['foo', 'bar', 'baz', 'quux'];
+echo $foo->$bar . "\n";
+echo $foo->{$baz[1]} . "\n";
+
+$start = 'b';
+$end   = 'ar';
+echo $foo->{$start . $end} . "\n";
+
+$arr = 'arr';
+echo $foo->{$arr[1]} . "\n";
+echo $foo->{$arr}[1] . "\n";
+
+$thiIsPhpLesson = true;
+
+echo $thiIsPhpLesson . PHP_EOL;
+echo PHP_EOL;
+$thiIsPhpLesson = boolval(1);
+
+var_dump($thiIsPhpLesson);
+
+
+?>
